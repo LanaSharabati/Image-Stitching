2672fa296d05b19382754f8890de9e91aa67d7fd
LanaSharabati <lanasharabati99@gmail.com>
2022-12-25 00:55:28 +0200

    .
    .

--- untitled2.py
+++ untitled2.py
@@ -1,88 +1,122 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.linalg import norm
-# img = cv.imread('Fr01.JPG')
-# img2 = cv.imread('Fr02.JPG')
-img = cv.imread('eifel.JPG')
-img2 = cv.imread('eifel02.JPG')
-# img = cv.imread('Jerusalem01.JPG')
-# img2 = cv.imread('Jerusalem02.JPG')
-gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-gray2 = cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
+import glob
 
-# plt.imshow(gray)
-# plt.imshow(gray2)
-up1 =gray[0]
-down1 = gray[len(gray)-1]
-left1 =[]
-right1=[]
-up2 =gray2[0]
-down2 = gray2[len(gray2)-1]
-
-left2 =[]
-right2=[]
-
-for i in range(len(gray)):
-    left1.append(gray[i][0])
-    right1.append(gray[i][len(gray[i])-1])
+class stitching:
+    def __init__(self,img1,img2):
+        
+        self.image1 = img1
+        self.image2 = img2
     
-for i in range(len(gray2)):
-    left2.append(gray2[i][0])
-    right2.append(gray2[i][len(gray2[i])-1])
+    
+    def edges_detect(self,img):
+        """
+        detect the image edges right , left ,up ,down
+
+        """
+        up =img[0]
+        down = img[len(img)-1]
+        left =[]
+        right=[]
+        for i in range(len(img)):
+            left.append(img[i][0])
+            right.append(img[i][len(img[i])-1])
+        return(up,down, np.array(right), np.array(left))
 
 
-
-# cosine = np.dot(up1,down2)/(norm(up1)*norm(down2))
-# cosine2 = np.dot(up2,down1)/(norm(up2)*norm(down1)) 
-# cosine3 = np.dot(np.array(right1),np.array(left2))/(norm(np.array(right1))*norm(np.array(left2))) 
-# cosine4 = np.dot(np.array(left1),np.array(right2))/(norm(np.array(right2))*norm(np.array(left1)))     
-
-# cos = np.dot(up1,down2) / (np.sqrt(np.dot(up1,up1)) * np.sqrt(np.dot(down2,down2)))
-# cos2 = np.dot(up2,down1) / (np.sqrt(np.dot(up2,up2)) * np.sqrt(np.dot(down1,down1)))
-# cos3 = np.dot(np.array(right2),np.array(left1)) / (np.sqrt(np.dot(np.array(right2),np.array(right2))) * np.sqrt(np.dot(np.array(left1),np.array(left1))))
-# cos4 = np.dot(np.array(right1),np.array(left2)) / (np.sqrt(np.dot(np.array(right1),np.array(right1))) * np.sqrt(np.dot(np.array(left2),np.array(left2))))
-
-cosine = up1 - down2
-cosine2 = up2-down1 
-# cosine3 = np.array(right1) - np.array(left2)
-# cosine4 = np.array(right2) - np.array(left1) 
-cosine3 = 0
-cosine4 = 0
-f = np.where(cosine == 0)[0]
-f2 = np.where(cosine2 == 0)[0]
-f3 = np.where(cosine3 == 0)[0]
-f4 = np.where(cosine4 == 0)[0]
-
-co = sum((up1 - down2)**2)/len(up1)
-co2 = sum((up2 - down1)**2)/len(up2)
-# co3 = sum((np.array(right1) - np.array(left2))**2)/len(right1)
-# co4 = sum((np.array(right2) - np.array(left1))**2)/len(right2) 
+    def  mean_square_error(self,list1,list2):
+        """
+        calculate the mean square erro to find 
+        the best place for image stitching
+        the up edges for first image with down edge foe second image viavesr
+        the left edge for first with righr with second
+        
+        """
+        mqe = sum((list1 - list2)**2)/len(list1)
+        return mqe
 
 
-e =[len(f),len(f2),len(f3),len(f4)] 
+    def image_stitching(self,lis,img1,img2):
+        """
+        compute the minumum value of mean square error for all edges
+        to detect the side nees to stitching
+        """
+        
+        ind = lis.index(min(lis))
+        
+        if  ind == 2:
+            first = img1
+            second = img2
+            n =1
+        elif ind == 0 :
+            first = img2
+            second = img1
+            n =0
+        elif ind == 1 :
+            first = img1
+            second = img2
+            n =0
+        elif ind == 3 :
+            first = img2
+            second = img1
+            n =1
+        con = np.concatenate((first, second ), axis=n)
+        cv.imwrite('out.png', con)
+        plt.imshow(con)
+        plt.figure() 
+        
+        
+def image_list():
+    """
+    Return a list images in test file
+    """
+    # os.chdir(folder_name) 
+    images = glob.glob('*.jpg')
+    return images
 
-ind = e.index( max(e)) 
-if  ind == 2: ##jur $ efil
-    first = img
-    second = img2
-    n =1
-elif ind == 0 :
-    first = img2
-    second = img
-    n =0
-elif ind == 1 :
-    first = img
-    second = img2
-    n =0
-elif ind == 3 :
-    first = img2
-    second = img
-    n =1
-  
+
+def read(image):
+    '''
+    take list of images name and read them
+
+    '''
+    img = cv.imread(image)
+    
+    return img
 
 
+    
+images = image_list()
 
-
-vis = np.concatenate((first, second ), axis=n)
-cv.imwrite('out.png', vis)
+for i in range(0,len(images),2):
+    
+    img1 = read(images[i])#read the first half
+    img2 = read(images[i+1])#read the second half
+    gray1 = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)#conver images to grayscale
+    gray2 =cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
+    class1 = stitching(img1, img2)
+    edges1 =  class1.edges_detect(gray1)
+    edges2 =  class1.edges_detect(gray2)
+    mean_values =[]
+    for i in range(0,len(edges1),2):
+        if len(edges1[i]) == len(edges2[i]):
+            error = class1.mean_square_error(edges1[i],edges2[i+1])
+            mean_values.append(error)
+            error = class1.mean_square_error(edges1[i+1],edges2[i])
+            mean_values.append(error)
+        else:
+            error = 1000000
+            mean_values.append(error)
+    print(mean_values)
+    class1.image_stitching(mean_values,img1,img2)
+        
+            
+    
+   
+    
+    
+    
+    
+    
+    
